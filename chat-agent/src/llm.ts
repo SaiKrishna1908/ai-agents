4bf2a6b3a58d8ba804bba4f6fe48a573f9cbc228
saikrishna1908 <saikrishna1908@gmail.com>
2025-06-15 15:04:24 -0500

    add memory to chat-agent

--- chat-agent/src/llm.ts
+++ chat-agent/src/llm.ts
@@ -1,16 +1,14 @@
+import type { AIMessage } from "../types";
 import { openai } from "./ai";
 
 
 export const runLLM = async ({
-    userMessage 
-} : {userMessage: string}) => {
+    messages 
+} : {messages: AIMessage[]}) => {
     const response = await openai.chat.completions.create({
         model: 'gpt-4o-mini',
-        temperature: 0.1,
-        messages: [{
-            role: 'user',
-            content: userMessage
-        }]
+        temperature: 1.0,
+        messages: messages
     });
 
     return response.choices[0].message.content;
