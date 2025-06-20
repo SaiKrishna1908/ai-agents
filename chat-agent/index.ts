10016ecb184ac6275dca68674d553b3bff537ac0
saikrishna1908 <saikrishna1908@gmail.com>
2025-06-20 16:55:25 -0500

    update agent to use tools

--- chat-agent/index.ts
+++ chat-agent/index.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config'
 import { runLLM } from './src/llm'
 import { addMessages, getMessages } from './src/memory'
+import { runAgent } from './src/agent'
+import { z } from 'zod'
 
 const userMessage = process.argv[2]
 
@@ -9,11 +11,30 @@ if (!userMessage) {
   process.exit(1)
 }
 
-await addMessages([{role: 'user', content: userMessage}])
-const messages = await getMessages()
+// give it a tool
+// const getWeather = async () => 'its hot, 89 deg'
 
-const response  = await runLLM({messages: [...messages, {role: 'user', content: userMessage}]});
+const weatherTool = {
+  name: 'get_stuff',
+  description: `use this to get weather`,
+  parameters: z.object({
+    reasoning: z.string().describe("why did you pick this tool>")
+  })
+}
+
+
+const response = await runAgent({userMessage, tools: [weatherTool]})
 
 console.log(response)
 
-await addMessages([{role: 'assistant', content: response}])
+// await addMessages([{ role: 'user', content: userMessage }])
+// const messages = await getMessages()
+
+// const response = await runLLM({
+//   messages: [...messages, { role: 'user', content: userMessage }],
+//   tools: [],
+// })
+
+// console.log(response)
+
+// await addMessages([{ role: 'assistant', content: response }])
--- chat-agent/src/llm.ts
+++ chat-agent/src/llm.ts
@@ -1,15 +1,17 @@
-import type { AIMessage } from "../types";
-import { openai } from "./ai";
+import type { AIMessage } from '../types'
+import { openai } from './ai'
+import { zodFunction } from 'openai/helpers/zod'
 
+export const runLLM = async ({ messages, tools }: { messages: AIMessage[], tools: any[] }) => {
+  const formattedTools = tools.map(zodFunction)
+  const response = await openai.chat.completions.create({
+    model: 'gpt-4o-mini',
+    temperature: 1.0,
+    messages: messages,
+    tools: formattedTools,
+    tool_choice: 'auto',
+    parallel_tool_calls: false
+  })
 
-export const runLLM = async ({
-    messages 
-} : {messages: AIMessage[]}) => {
-    const response = await openai.chat.completions.create({
-        model: 'gpt-4o-mini',
-        temperature: 1.0,
-        messages: messages
-    });
-
-    return response.choices[0].message.content;
+  return response.choices[0].message
 }
